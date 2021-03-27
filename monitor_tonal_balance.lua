f8cd77faecc9046a5eea5b34d3e867a5683e9754
Patricio <patomaripani@gmail.com>
2021-03-26 23:16:54 -0300

    v8

---
+++ monitor_tonal_balance.lua
@@ -0,0 +1,12 @@
+local POS_TONAL = 2
+local track = reaper.GetMasterTrack()
+POS_TONAL = (0x1000000 + (POS_TONAL-1))
+
+
+if reaper.TrackFX_GetOpen(track,POS_TONAL) == false then
+--reaper.ShowConsoleMsg("if")
+reaper.TrackFX_Show(reaper.GetMasterTrack(), POS_TONAL, 1)
+else 
+--reaper.ShowConsoleMsg("else")
+reaper.TrackFX_Show(reaper.GetMasterTrack(), POS_TONAL, 0)
+end 
