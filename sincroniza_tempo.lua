e1317714dd52aff144d57b6fb89919f196658ac7
Patricio <patomaripani@gmail.com>
2021-04-03 20:41:55 -0300

    sincroniza tempo para delays o release

---
+++ sincroniza_tempo.lua
@@ -0,0 +1,12 @@
+local tempo=reaper.Master_GetTempo()
+
+
+reaper.ShowMessageBox(
+"BPM: "..tempo.."\n"
+.."1/1: "..60000/tempo.."\n"
+.."1/2: "..60000/(tempo*2).."\n"
+.."1/4: "..60000/(tempo*4).."\n"
+.."1/8: "..60000/(tempo*8).."\n"
+.."1/16: "..60000/(tempo*16).."\n"
+.."1/32: "..60000/(tempo*32).."\n"
+,'Sincroniza tu tiempo',0)
